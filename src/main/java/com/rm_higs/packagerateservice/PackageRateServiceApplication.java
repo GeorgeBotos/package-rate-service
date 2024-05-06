701005a054f2af208751776e732297aac74fe5f0
george.botos.no1@gmail.com <george.botos.no1@gmail.com>
2024-05-06 15:00:14 +0100

    Initial Commit

---
+++ src/main/java/com/rm_higs/packagerateservice/PackageRateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rm_higs.packagerateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageRateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageRateServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rm_higs/packagerateservice/PackageRateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm_higs.packagerateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageRateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
